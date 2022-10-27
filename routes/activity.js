d032a4257fe3c6c1640766a1d3a5f9df2c631c9a
arthurgeay <arthurgeay.contact@gmail.com>
2022-10-27 17:53:15 +0200

    update route activity

--- routes/activity.js
+++ routes/activity.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const activityCtrl = require("../controllers/activity");
 
 router.get("/users/:userId", activityCtrl.getActivities);
-router.get("/users/:userId/:activityId", activityCtrl.getActivity);
+router.get("/:activityId/users/:userId", activityCtrl.getActivity);
 router.post("/users/:userId", activityCtrl.createActivity);
 router.put("/:activityId/users/:userId", activityCtrl.updateActivity);
 router.delete("/:activityId/users/:userId", activityCtrl.deleteActivity);
