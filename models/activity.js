1e443685023da15fbe84a37b4ab6e9ffa8b0e6ee
arthurgeay <arthurgeay.contact@gmail.com>
2022-10-27 15:17:45 +0200

    add activity model

---
+++ models/activity.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  type: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true },
+  calories: { type: Number, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  distance: { type: Number, required: true },
+  averageHeartRate: { type: Number, required: true },
+  maxHeartRate: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
